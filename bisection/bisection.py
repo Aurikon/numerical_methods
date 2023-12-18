1e0ba4128c1febb1b0d3baaf1ac7b060e832b085
Aurikon <watarai775@gmail.com>
2023-12-18 17:49:42 +0400

    add bisection

---
+++ bisection/bisection.py
@@ -0,0 +1,20 @@
+def f(x):
+    print("If x ", x, " fx: ", x ** 3 + 4 * x ** 2 - 10)
+    return x ** 3 + 4 * x ** 2 - 10
+
+
+def bisection(f, a, b, epsilon=1e-5):
+    count = 0
+    c = a + ((b - a) / 2)
+    while abs(f(c)) > epsilon:
+        count += 1
+        if f(c) * f(a) < 0:
+            b = c
+        else:
+            a = c
+        c = a + ((b - a) / 2)
+
+    return c, count
+
+
+print(bisection(f, 1, 2))
